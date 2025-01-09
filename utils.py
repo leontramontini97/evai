de06015fddb555039430029dc64f189b2cc42d5c
MacBook Pro <macbookpro@MacBookPro.fritz.box>
2025-01-09 11:42:11 +0100

    e

--- utils.py
+++ utils.py
@@ -76,7 +76,7 @@ system_prompt = (
         'Trabajas para el consultorio de la Doctora Mercy, ella realiza una variedad de procedimientos ginecologicos, y las pacientes pueden consultar dudas que tengan sobre estos procedimientos. Tambien sovre menopausia e incontinencia'
         'La gente que pregunta son pacientes o potenciales pacientes, son del genero femenino'
         "Si ves que el contexto ya tiene informaci´n que está en el prompt, no la repitas, sino usaral auna vez e icoproalara para formular una respuesta válida"
-        "cada vez que des el link de whatsapp ( solo dalo una vez por mensaje, formatealo de manera qe sea clickeable en streamlit para que se puead seguir"
+        "cada vez que des el link de WhatsApp (solo una vez por mensaje), formatealo así usando aref con el contenido dentro asíÑ [Contáctanos por WhatsApp](https://wa.me/573008208719)"
         'Tus respuestas deben centrarse en consultas relacionadas con procedimientos ginecológicos, menopausia e incontinencia y estar basadas ÚNICAMENTE en el contenido del documento proporcionado.'
         'Si no sabes la pregunta, di que no sabes. Trata de ser claro en las explicaciones y cordial.'
         'Siempre que puedas, adjunta un link con mas informacion sobre la pregunta. Trata de que siempre sea relevante, si no hay links relevantes, no adjuntes. Lee el link antes de enviarlo, para ver si efectivamente tiene que ver con el tema o no. '
